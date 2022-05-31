5eee8b6ed8ee360411aef6d0910c3af8855fc5e4
Zach <zachsneeringerdev@gmail.com>
2022-05-31 01:10:07 -0400

    Finished!! UI and functionality

--- etch.js
+++ etch.js
@@ -1,23 +1,52 @@
 
 // Etch-a-Sketch
 
-let xValue = document.querySelector('.x');
-let yValue = document.querySelector('.y');
 const inputSlider = document.querySelector('[type="range"]');
 const colorPicker = document.querySelector('[type="color"]');
 const cPickerOverlay = document.querySelector('label');
 const clearButton = document.querySelector('.clear');
 const eraseButton = document.querySelector('.erase');
 const rgbButton = document.querySelector('.rgb');
-const allBoxes = Array.from(document.querySelectorAll('.screen > div'));
+let allBoxes = Array.from(document.querySelectorAll('.screen > div'));
 let activeBtn = false;
 let mousePressed = false;
 
+// ------------------------------------ Top Function Bar
+
+const pushToGrid = (num2push, SCREEN) => {
+    for (let z = 0; z < num2push; z++) {
+        const newBox = document.createElement('div');
+        newBox.style.border = '1px solid gray';
+        SCREEN.append(newBox);
+        allBoxes.push(newBox);
+    }
+    allBoxes.forEach(div => div.addEventListener('mousedown', handleMouseD));
+    if (rgbButton.classList.contains('active')) {
+        allBoxes.forEach(div => div.addEventListener('mousedown', rgbDown));
+    }
+}
+
+const updateGrid = (num) => {
+    const SCREEN = document.querySelector('.screen');
+    const num2push = num * num;
+    SCREEN.innerHTML = '';
+    allBoxes = [];
+    SCREEN.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    SCREEN.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    pushToGrid(num2push, SCREEN);
+}
+
 const updateXY = (e) => {
+    let xValue = document.querySelector('.x');
+    let yValue = document.querySelector('.y');
+    let num = e.target.value;
     e.preventDefault();
     xValue.textContent = e.target.value;
     yValue.textContent = e.target.value;
+    updateGrid(num);
 }
+
+// ------------------------------------ Bottom Function Bar
 
 const changeColor = (e) => {
     eraseButton.classList.remove('active');
@@ -39,12 +68,9 @@ const handleMouseM = (e) => {
 
 const handleMouseD = (e) => {
     if (!activeBtn) {
-        console.log('uh');
-        console.log('Active is false!! :o');
         e.preventDefault();
         e.target.style.background = window.getComputedStyle(cPickerOverlay).background;
         allBoxes.forEach(div => div.addEventListener('mousemove', handleMouseM));
-        console.log('handling mouse D!');
     } else return;
 }
 
@@ -62,7 +88,6 @@ const stopEraser = (e) => {
 }
 
 const eraseMove = (e) => {
-    console.log('moving haaaaaaaa');
     e.target.style.background = 'none';
     allBoxes.forEach(div => div.addEventListener('mouseup', stopEraser));
 }
@@ -113,7 +138,7 @@ const rgbDraw = () => {
     allBoxes.forEach(div => div.addEventListener('mousedown', rgbDown));
 }
 
-
+// ------------------------------------ Event Listeners
 
 inputSlider.addEventListener('input', updateXY);
 colorPicker.addEventListener('input', changeColor);
